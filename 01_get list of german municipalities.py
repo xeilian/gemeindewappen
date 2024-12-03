4283c8eedffee7291d56128d2d735856c7b07372
xeilian <quqiuh@gmail.com>
2024-12-03 01:55:16 +0100

    Massively improved municipalities script

--- 01_get list of german municipalities.py
+++ 01_get list of german municipalities.py
@@ -22,14 +22,51 @@ wikidata_bundeslaender = {
 }
 
 
-def extract_alle_gemeinden(bundesland_wikidata_id):
+def extract_alle_landkreise(bundesland_wikidata_id):
+    sparql_query = f"""
+    SELECT DISTINCT ?item ?itemLabel WHERE {{
+        SERVICE wikibase:label {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE]". }}
+        ?item p:P31 ?statement0.
+        ?statement0 (ps:P31/(wdt:P279*)) wd:Q106658.
+        ?item p:P131 ?statement1.
+        ?statement1 (ps:P131/(wdt:P131*)) wd:{bundesland_wikidata_id}.
+    }}
+    LIMIT 100
+    """
+
+    sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+    sparql.setQuery(sparql_query)
+    sparql.setReturnFormat(JSON)
+    
+    print(f"[Landkreise] SPARQL-Query für {wikidata_bundeslaender[bundesland_wikidata_id]} gesendet!")
+
+    try:
+        results = sparql.query().convert()
+        with open(f"gemeinden_deutschland\landkreise_deutschland.csv", mode="a", newline="", encoding="utf-8") as file:
+            writer = csv.writer(file)
+            writer.writerow(["Wikidata-Link", "Name"])
+            
+            landkreis_ids = []
+            for result in results["results"]["bindings"]:
+                item = result['item']['value']
+                label = result['itemLabel']['value']
+                writer.writerow([wikidata_bundeslaender[bundesland_wikidata_id], item, label])
+                landkreis_ids.append(item)
+        print(f"Die Landkreise für {wikidata_bundeslaender[bundesland_wikidata_id]} wurden verarbeitet!")
+        return landkreis_ids
+    except Exception as e:
+        print(f"Fehler beim Abrufen oder Speichern von Landkreisdaten für {wikidata_bundeslaender[bundesland_wikidata_id]}: {e}")
+        return None
+
+
+def extract_alle_gemeinden(landkreis_wikidata_id, bundesland_wikidata_id):
     sparql_query = f"""
     SELECT DISTINCT ?item ?itemLabel WHERE {{
         SERVICE wikibase:label {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE]". }}
         ?item p:P31 ?statement0.
         ?statement0 (ps:P31/(wdt:P279*)) wd:Q262166.
         ?item p:P131 ?statement1.
-        ?statement1 (ps:P131/(wdt:P131*)) wd:{bundesland_wikidata_id}.
+        ?statement1 (ps:P131/(wdt:P131*)) wd:{landkreis_wikidata_id}.
     }}
     LIMIT 100
     """
@@ -37,11 +74,11 @@ def extract_alle_gemeinden(bundesland_wikidata_id):
     sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
     sparql.setQuery(sparql_query)
     sparql.setReturnFormat(JSON)
-    print(f"SPARQL-Query für {wikidata_bundeslaender[bundesland_wikidata_id]} gesendet!")
+    print(f"[Gemeinden] SPARQL-Query für {wikidata_bundeslaender[bundesland_wikidata_id]} gesendet!")
 
     try:
         results = sparql.query().convert()
-        with open(f"gemeinden_deutschland/{wikidata_bundeslaender[bundesland_wikidata_id].lower().replace(" ", "_").replace("ü","ue")}_gemeinden.csv", mode="w", newline="", encoding="utf-8") as file:
+        with open(f"gemeinden_deutschland\{wikidata_bundeslaender[bundesland_wikidata_id].lower().replace(" ", "_").replace("ü","ue")}_gemeinden.csv", mode="w", newline="", encoding="utf-8") as file:
             writer = csv.writer(file)
             writer.writerow(["Wikidata-Link", "Name"])
             
@@ -49,13 +86,13 @@ def extract_alle_gemeinden(bundesland_wikidata_id):
                 item = result['item']['value']
                 label = result['itemLabel']['value']
                 writer.writerow([item, label])
-        print(f"{wikidata_bundeslaender[bundesland_wikidata_id]} wurde verarbeitet!")
+        print(f"Die Gemeinden für {wikidata_bundeslaender[bundesland_wikidata_id]} wurde verarbeitet!")
     except Exception as e:
         print(f"Fehler beim Abrufen oder Speichern von Daten für {wikidata_bundeslaender[bundesland_wikidata_id]}: {e}")
 
 
 if __name__ == "__main__":
     for i in wikidata_bundeslaender.keys():
-        extract_alle_gemeinden(i)
-        for i in tqdm.tqdm(range(50)):
-            time.sleep(1)
+        list_landkreise = extract_alle_landkreise(i)
+        for j in list_landkreise:
+            extract_alle_gemeinden(j, i)
