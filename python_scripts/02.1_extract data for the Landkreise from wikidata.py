ce4a077e55a600f03a42e7771412b111c68ee733
xeilian <quqiuh@gmail.com>
2024-12-05 10:04:34 +0100

    Improved script

--- python_scripts/02.1_extract data for the Landkreise from wikidata.py
+++ python_scripts/02.1_extract data for the Landkreise from wikidata.py
@@ -3,17 +3,27 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 sparql_query = """
 SELECT DISTINCT ?instanceOf ?capital ?adminUnit ?coordinates ?population ?subdivision 
                 ?area ?coatOfArms ?insignia ?gnd ?geonamesID ?openStreetMapRelationID ?openStreetMapNodeID
-                ?startDate ?endDate WHERE {
+                ?popStartDate ?areaStartDate ?instanceOfStartDate ?adminUnitStartDate ?adminUnitEndDate WHERE {
   
     # Instanz von (P31)
-    OPTIONAL { wd:Q3232 wdt:P31 ?instanceOf. }
-    
+    OPTIONAL {
+      wd:Q3232 p:31 ?instanceOfStatement.
+      ?instanceOfStatement ps:P31 ?instanceOf.
+      OPTIONAL { ?instanceOfStatement pq:P585 ?instanceOfStartDate. }
+    }
+
     # Hauptstadt (P36)
     OPTIONAL { wd:Q3232 wdt:P36 ?capital. }
     
     # Verwaltungseinheit (P131)
-    OPTIONAL { wd:Q3232 wdt:P131 ?adminUnit. }
-    
+    OPTIONAL {
+      wd:Q3232 p:131 ?adminUnitStatement.
+      ?adminUnitStatement ps:P131 ?adminUnit.
+      OPTIONAL { ?adminUnitStatement pq:P585 ?adminUnitStartDate. }
+      OPTIONAL { ?adminUnitStatement pq:P582 ?adminUnitEndDate. }
+
+    }
+
     # Koordinaten (P625)
     OPTIONAL { wd:Q3232 wdt:P625 ?coordinates. }
     
@@ -21,15 +31,18 @@ SELECT DISTINCT ?instanceOf ?capital ?adminUnit ?coordinates ?population ?subdiv
     OPTIONAL {
       wd:Q3232 p:P1082 ?populationStatement.
       ?populationStatement ps:P1082 ?population.
-      OPTIONAL { ?populationStatement pq:P585 ?startDate. }
-      OPTIONAL { ?populationStatement pq:P582 ?endDate. }
+      OPTIONAL { ?populationStatement pq:P585 ?popStartDate. }
     }
     
     # Untereinheiten (P150)
     OPTIONAL { wd:Q3232 wdt:P150 ?subdivision. }
     
     # Fläche (P2046)
-    OPTIONAL { wd:Q3232 wdt:P2046 ?area. }
+    OPTIONAL {
+      wd:Q3232 p:P2046 ?areaStatement.
+      ?areaStatement ps:P1082 ?area.
+      OPTIONAL { ?areaStatement pq:P585 ?areaStartDate. }
+    }
     
     # Wappen (P94)
     OPTIONAL { wd:Q3232 wdt:P94 ?coatOfArms. }
