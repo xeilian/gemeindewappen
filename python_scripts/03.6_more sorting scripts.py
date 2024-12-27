c1972df43bc13effc86a18b2ab466a738079b3c9
xeilian <quqiuh@gmail.com>
2024-12-28 00:11:26 +0100

    New sorting script that sorts out specific entities from a csv file

---
+++ python_scripts/03.6_more sorting scripts.py
@@ -0,0 +1,28 @@
+import csv, re
+
+def sort_specific_categories_out():
+    with open('wikidata_output/gemeinden_deutschland.csv', mode='r', encoding="utf-8", newline="") as file:
+        reader = csv.reader(file)
+        rows = list(reader)
+        verwaltungsgemeinden = []
+
+        for row in rows:
+            ids = re.findall(r'Q\d+', row[1])
+            if "Q41762994" in ids:
+                verwaltungsgemeinden.append(row)
+                rows.remove(row)
+        
+        with open('wikidata_output/gemeinden_deutschland.csv', mode='w', encoding='utf-8', newline='') as file1:
+            writer1 = csv.writer(file1)
+            for i in rows:
+                writer1.writerow(i)
+
+        with open('wikidata_output/verwaltungsgemeinden_deutschland.csv', mode='a', encoding='utf-8', newline='') as file2:
+            writer2 = csv.writer(file2)
+            for i in verwaltungsgemeinden:
+                writer2.writerow(i)
+
+if __name__ == "__main__":
+    sort_specific_categories_out()
+
+# 1. verwaltungsgemeinden (Q15725618, Q41762994)
