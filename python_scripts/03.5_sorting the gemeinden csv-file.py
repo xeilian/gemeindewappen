ad95ff1c9d67b42e63221a579502c919137383f6
xeilian <quqiuh@gmail.com>
2024-12-27 23:09:09 +0100

    Improved, sorted csv files

--- python_scripts/03.5_sorting the gemeinden csv-file.py
+++ python_scripts/03.5_sorting the gemeinden csv-file.py
@@ -102,13 +102,13 @@ def sort_into_the_new_csv_files():
             ids = re.findall(r'Q\d+', i[1])
             coa_image = re.findall(r'http://commons.wikimedia.org/', i[11])
 
-            if i[24] != "":
+            if i[24] != "" or i[25] != "":
                 with open('wikidata_output/gemeinden_deutschland.csv', encoding="utf-8", mode="a", newline="") as gemeindefile:
                     gemeinde_writer = csv.writer(gemeindefile)
                     gemeinde_writer.writerow(i)
             elif (
                 any(item in ids for item in ['Q116457956', 'Q42744322', 'Q253019', 'Q1138414', 'Q532', 'Q5084', 'Q486972', 'Q262166', 'Q123705', 'Q10354598']) 
-                and i[24] == "") or "http://commons.wikimedia.org/" in coa_image:
+                and (i[24] == "" or i[25] == "")) or "http://commons.wikimedia.org/" in coa_image:
                 with open('wikidata_output/siedlungen_ortsteile_deutschland.csv', encoding="utf-8", mode="a", newline="") as ortsteilefile:
                     ortsteile_writer = csv.writer(ortsteilefile)
                     ortsteile_writer.writerow(i)
@@ -123,4 +123,3 @@ if __name__ == "__main__":
     #new_column()
     #fetch_municipality_keys()
     sort_into_the_new_csv_files()
-    pass
