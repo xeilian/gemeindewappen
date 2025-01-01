2f055e0eb4a730c0490066c49bf41a616de751e1
xeilian <quqiuh@gmail.com>
2025-01-01 17:18:18 +0100

    New map feature

--- django_scripts/gemeindewappen_website/urls.py
+++ django_scripts/gemeindewappen_website/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.landkreise_list, name='landkreise_list'),  # Root-URL
+    path('', views.landkreise_list, name='landkreise_list'), 
     path('landkreise/<str:wikidata_id>/', views.landkreis_detail, name='landkreis_detail'),
+    path('map/', views.maps, name='map')
 ]
--- django_scripts/gemeindewappen_website/views.py
+++ django_scripts/gemeindewappen_website/views.py
@@ -1,16 +1,19 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from .models import Wappen, Landkreis
 
 # Create your views here.
 
 def wappen_list(request):
-    wappen_list = Wappen.objects.all()  # Alle Wappen abrufen
+    wappen_list = Wappen.objects.all()  
     return render(request, 'wappen_list.html', {'wappen_list': wappen_list})
 
 def landkreise_list(request):
-    landkreise = Landkreis.objects.all()  # Ensure this fetches all valid Landkreise
+    landkreise = Landkreis.objects.all() 
     return render(request, 'gemeindewappen_website/landkreise_list.html', {'landkreise': landkreise})
 
 def landkreis_detail(request, wikidata_id):
     landkreis = Landkreis.objects.get(wikidata_id=wikidata_id)
     return render(request, 'gemeindewappen_website/landkreis_detail.html', {'landkreis': landkreis})
+
+def maps(request):
+    return render(request, 'gemeindewappen_website/map.html')
